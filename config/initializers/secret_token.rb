c5b19120e178a17979a21191a6c53d140873042b
jimxmeter@gmail.com <jimxmeter@gmail.com>
2013-11-07 11:52:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rebelrat::Application.config.secret_key_base = '149f343d6af7951399a557b62264dbdb70f885d8d48ce70e480c17cdf546895daa4a9b64afe23852f3082ee5df24bb1647c6fe83dec863669f070ec72014fdbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rebelrat::Application.config.session_store :cookie_store, key: '_rebelrat_session'
