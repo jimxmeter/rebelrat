1177d0122ca53f62f0cd274c9b555c964d5048a9
jimxmeter@gmail.com <jimxmeter@gmail.com>
2013-11-11 21:12:36 -0500

    Edited bootstrap for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,74 +1,67 @@
-class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  class PinsController < ApplicationController
+    before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
-  # GET /pins.json
-  def index
-    @pins = Pin.all
-  end
+   
+    def index
+      @pins = Pin.all
+    end
 
-  # GET /pins/1
-  # GET /pins/1.json
-  def show
-  end
+    def show
+    end
 
-  # GET /pins/new
-  def new
-    @pin = Pin.new
-  end
 
-  # GET /pins/1/edit
-  def edit
-  end
+    def new
+      @pin = Pin.new
+    end
 
-  # POST /pins
-  # POST /pins.json
-  def create
-    @pin = Pin.new(pin_params)
+   
+    def edit
+    end
 
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+   
+    def create
+      @pin = Pin.new(pin_params)
+
+      respond_to do |format|
+        if @pin.save
+          format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+          format.json { render action: 'show', status: :created, location: @pin }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @pin.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
-  def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+
+    def update
+      respond_to do |format|
+        if @pin.update(pin_params)
+          format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @pin.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @pin.destroy
+      respond_to do |format|
+        format.html { redirect_to pins_url }
         format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
     end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_pin
+        @pin = Pin.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def pin_params
+        params.require(:pin).permit(:description, :string)
+      end
   end
-
-  # DELETE /pins/1
-  # DELETE /pins/1.json
-  def destroy
-    @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description, :string)
-    end
-end
