a1707b34adfabf46486b9a20aebae81540f30caf
jimxmeter@gmail.com <jimxmeter@gmail.com>
2013-11-15 18:20:58 -0500

    reset2 push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rebelrat::Application.routes.draw do
+  
+  devise_for :users
   root "pages#home"
-  get "about" => "pages#about"
+  get "music" => "pages#music"
+  get "life" => "pages#life"
+  get "style" => "pages#style"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
