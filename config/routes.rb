efce6afde03beff300678f34f3ece114e3a9c1f3
jimxmeter@gmail.com <jimxmeter@gmail.com>
2013-11-15 18:23:02 -0500

    reset3 to push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rebelrat::Application.routes.draw do
   
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "music" => "pages#music"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111214057) do
+ActiveRecord::Schema.define(version: 20131115051556) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
