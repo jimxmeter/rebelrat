fcd26d5c56b768d326dbc602b732046f98eb2741
jimxmeter@gmail.com <jimxmeter@gmail.com>
2013-11-15 18:31:51 -0500

    Add Amazon S3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Rebelrat::Application.configure do
 
   # Required for heroku
   config.action_mailer.default_url_options = { :host => 'http://whitegirlfm.herokuapp.com/' }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
